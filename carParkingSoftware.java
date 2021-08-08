41ed8004d788acdf3771b8c3f66e872d9e0ff3f8
Shahnawaz Jawed <66067618+Jawed02@users.noreply.github.com>
2021-08-08 11:03:55 +0530

    Add files via upload

---
+++ carParkingSoftware.java
@@ -0,0 +1,58 @@
+package com.practices;
+
+import java.io.*;
+
+
+
+/**
+ * Date: April 13-2021
+ * This is the simple program for car parking
+ * @author Shahnawaz Jawed
+ *@version 1.0
+ *
+ */
+public class carParkingSoftware {
+	
+	
+	
+	
+	/**
+	 * This methods return the amount
+	 * @param amount parameter for amount
+	 */
+	public void print(int amount) {
+		System.out.println("Your Bill is $"+amount);
+	}
+	public void print(double amount) {
+		System.out.println("Your Bill is $"+amount);
+	}
+
+	public static void main(String[] args) throws Exception, IOException {
+		
+		carParkingSoftware cps = new carParkingSoftware();
+	    int bill;
+		double a = 5;
+		int b = 1;
+		double c = 0.5;
+	    int f = 24;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Welcome to car parking Software");
+        System.out.println("Enter the time");
+        int hr = Integer.parseInt(br.readLine());
+                if(hr<= 5) {
+        	bill = b*hr;
+        	cps.print(bill);
+        }else if(hr>5 && hr<24) {
+        	double d = hr-a;
+            double e = 5+(c*d);
+        	cps.print(e);
+        }else if(hr>=f) {
+            int g = hr-f;
+            double h = 15+(g*c);
+        cps.print(h);
+        }
+        
+   
+	}
+
+}
